575a24021e657a76ae53018160551fb905bdc12a
Приданников Михаил <p.mihail174@yandex.ru>
2026-01-22 16:34:15 +0500

    v1.0.0

---
+++ assets/js/custom_popup.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * Custom Popups 1.0.0
+ * Открытие модальных окон
+ *
+ * Copyright 2025 Mihail Pridannikov
+ *
+ * Released on: 2025
+ *
+ */
+
+const CustomPopup = function(settings) {
+    const BUTTON = document.querySelectorAll(settings.button);
+    const POPUP = document.querySelector(settings.popup);
+
+    this.init = function() {
+        BUTTON.forEach(button => {
+            button.addEventListener('click', e => {
+                e.preventDefault();
+                this.disableScrolling();
+                POPUP.classList.add('is-opened');
+            });
+        });
+        POPUP.querySelector('.popup__close').addEventListener('click', e => this.handlerOnClickButtonClose(e));
+        POPUP.querySelector('.popup__overlay').addEventListener('click', e => this.handlerOnClickOverlay(e));
+    }
+    this.handlerOnClickButtonClose = function() {
+        this.enableScrolling();
+        POPUP.classList.remove('is-opened');
+    }
+    this.handlerOnClickOverlay = function(e) {
+        this.enableScrolling();
+        POPUP.classList.remove('is-opened');
+    }
+    this.disableScrolling = function(e) {
+        document.body.classList.add('js-scroll-locked');
+    }
+    this.enableScrolling = function(e) {
+        document.body.classList.remove('js-scroll-locked');
+    }
+
+    this.init();
+}
